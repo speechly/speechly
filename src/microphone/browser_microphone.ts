99b9a2832fa002a85820bb90334f41abe3f32319
Igor Suleymanov <radiohead@users.noreply.github.com>
2020-06-03 09:40:00 +0300

    Improve resampling performance in Chrome and Firefox (#27)
    * Pass sample rate to AudioContext for native resampling
    
    * Skip resampling if source and target sample rates are equal
    
    * Remove debug output

--- src/microphone/browser_microphone.ts
+++ src/microphone/browser_microphone.ts
@@ -1,7 +1,7 @@
 import { ErrorCallback } from '../types'
 import { AudioCallback, Microphone } from './types'
 import { AudioProcessor, BrowserAudioProcessor } from './browser_audio_processor'
-import { AudioFilter, generateDownsampler, float32ToInt16 } from './downsampler'
+import { float32ToInt16, AudioFilter, newSampler } from './sampler'
 
 export class BrowserMicrophone implements Microphone {
   private readonly audioProcessor: AudioProcessor
@@ -10,9 +10,9 @@ export class BrowserMicrophone implements Microphone {
   private isMuted: boolean = false
   private onAudioCb: AudioCallback = () => {}
 
-  constructor(sampleRate: number, audioProcessor?: AudioProcessor, downsampler?: AudioFilter) {
-    this.audioProcessor = audioProcessor ?? new BrowserAudioProcessor(this.handleAudio)
-    this.downsampler = downsampler ?? generateDownsampler(this.audioProcessor.sampleRate, sampleRate)
+  constructor(sampleRate: number, audioProcessor?: AudioProcessor, sampler?: AudioFilter) {
+    this.audioProcessor = audioProcessor ?? new BrowserAudioProcessor(sampleRate, this.handleAudio)
+    this.downsampler = sampler ?? newSampler(this.audioProcessor.sampleRate, sampleRate)
   }
 
   onAudio(cb: AudioCallback): void {
@@ -22,7 +22,10 @@ export class BrowserMicrophone implements Microphone {
   initialize(cb: ErrorCallback): void {
     this.audioProcessor
       .initialize()
-      .then(() => cb())
+      .then(() => {
+        this.mute()
+        cb()
+      })
       .catch(cb)
   }
 
