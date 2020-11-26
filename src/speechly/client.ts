6ca7eefeddac1dc67949b32b298803467c617131
Igor Poltavskiy <igorpoltavskiy@Igors-MacBook-Pro.local>
2020-11-26 09:55:46 +0200

    check nullable string explicitly

--- src/speechly/client.ts
+++ src/speechly/client.ts
@@ -76,7 +76,7 @@ export class Client {
   private intentCb: IntentCallback = () => {}
 
   constructor(options: ClientOptions) {
-    if (options.language && !localeCode.validate(options.language)) {
+    if (options.language != null && !localeCode.validate(options.language)) {
       throw Error(`[SpeechlyClient] Invalid language "${options.language}"`)
     }
 
