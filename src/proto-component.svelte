764ebab46ef9ef42166208b7bea1df47e3d509d8
Ari Nykänen <ari.o.nykanen@gmail.com>
2021-03-23 11:03:41 +0200

    Safari 12 testing

--- src/proto-component.svelte
+++ src/proto-component.svelte
@@ -3,6 +3,11 @@
 <script lang="ts">
   import { get_current_component } from "svelte/internal";
   import { onMount } from "svelte";
+  import type { ITaggedWord } from "./types";
+  import { fadeIn, interpolateLinearf } from "./TableInterpolator";
+  import fix from "./transFix";
+  import { cubicInOut } from "svelte/easing";
+  import { fade as fade_orig } from 'svelte/transition';
 
   const thisComponent = get_current_component();
   const dispatchUnbounded = (name: string, detail?: {}) => {
@@ -12,9 +17,33 @@
     }));
   };
 
-  // Prepare a dispatchUnbounded function to communicate outside shadow DOM box. Svelte native dispatchUnbounded won't do that.
+  const fade = fix(fade_orig);
 
-  //dispatchUnbounded("debug", "Hello from proto-component constructor 1");
+  const revealTransition = fix((node, {delay = 0, duration = 800}) => {
+    return {
+      delay,
+      duration,
+      easing: cubicInOut,
+      css: (t: number) => `
+        opacity: ${interpolateLinearf(fadeIn, t, 0.0, 1.0)};
+        max-height: ${interpolateLinearf(fadeIn, t, 0.0, 0.6) * 10}rem;
+        margin-bottom: ${interpolateLinearf(fadeIn, t, 0.0, 0.6) * 1.5}rem;
+      `
+    };
+  });
+
+  const slideTransition = fix((node, {delay = 0, duration = 250}) => {
+    return {
+      delay,
+      duration,
+      css: (t: number) => `
+        max-width: ${interpolateLinearf(fadeIn, t, 0.0, 1.0) * 10}rem;
+      `
+    };
+  });
+
+  let words: ITaggedWord[] = [{word: "Initializing", entityType: null, isFinal: true, serialNumber: 1}];
+  let visible = false;
 
   const pingHandler = (e) => {
     dispatchUnbounded("debug", "proto-component.ping 1");
