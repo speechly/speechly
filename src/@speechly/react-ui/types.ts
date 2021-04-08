b2ec6e3ec9fd5b3e6814a8ac7d9988e42ad28d2b
Ari Nykänen <ari.o.nykanen@gmail.com>
2021-04-08 17:29:33 +0300

    Removed local copy of react-ui

--- src/@speechly/react-ui/types.ts
+++
@@ -1,7 +0,0 @@
-export enum SpeechlyUiEvents {
-  TangentRelease = 'TangentRelease',
-  TangentPress = 'TangentPress',
-  Notification = 'Notification',
-  WarningNotification = 'WarningNotification',
-  DismissNotification = 'DismissNotification',
-};
