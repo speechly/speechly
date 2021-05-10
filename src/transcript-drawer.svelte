5b1e99ebdf48f6950f98aa9e5f214ef6156c6dc3
Ari Nykänen <ari.o.nykanen@gmail.com>
2021-05-10 16:42:44 +0300

    Improved transitions, configurable hint text

--- src/transcript-drawer.svelte
+++ src/transcript-drawer.svelte
@@ -1,26 +1,45 @@
 <svelte:options tag="transcript-drawer" immutable={true} />
 
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { cubicOut } from 'svelte/easing';
+  import { cubicIn, cubicOut, linear } from 'svelte/easing';
   import "./big-transcript.svelte";
   import { tweened } from 'svelte/motion';
 
-  let buttonheld = false;
-  let transition = tweened({ y: -1, opacity: 0 }, {
+  export let height = "8rem";
+  export let hint = `Try: "Show me blue jeans"`;
+
+  let positionTransition = tweened({ y: -1.0 }, {
     duration: 200,
-    easing: cubicOut,
   });
+
+  let opacityTransition = tweened({ opacity: 0 }, {
+    duration: 200,
+    easing: linear,
+  });
+
+  let hintTransition = tweened({ opacity: 0 }, {
+    duration: 200,
+    delay: 200,
+    easing: linear,
+  });
+
+  const bigTranscriptVisibilityChanged = (e) => {
+    const visibility = e.detail;
+    if (visibility === false) {
+      positionTransition.set({y: -1.0}, {easing: cubicIn});
+      opacityTransition.set({opacity: 0});
+      hintTransition.set({opacity: 0});
+    } else {
+      positionTransition.set({y: 0}, {easing: cubicOut});
+      opacityTransition.set({opacity: 1});
+      hintTransition.set({opacity: 1});
+    }
+  }
 
   const handleMessage = (e) => {
     switch (e.data.type) {
-      case "holdstart":
-        buttonheld = true;
-        transition.set({y: 0, opacity: 1});
-        break;
-      case "holdend":
-        buttonheld = false;
-        transition.set({y: -1, opacity: 0});
+      case "speechsegment":
+        hintTransition.set({opacity: 0});
         break;
       default:
         break;
@@ -33,12 +52,14 @@
   on:message={handleMessage}
 />
 
-<main class="placementTop">
-  <div class="drawer" style="opacity: {$transition.opacity}; transform: translate(0px, {$transition.y}rem);">
+<main class="placementTop" style="
+  --height: {height};
+">
+  <div class="drawer" style="opacity: {$opacityTransition.opacity}; transform: translate(0px, {$positionTransition.y}rem);">
     <div class="pad">
-      <big-transcript></big-transcript>
-      <div class="hint">
-        Try "Show me blue jeans"
+      <big-transcript on:visibilitychanged={bigTranscriptVisibilityChanged}></big-transcript>
+      <div class="hint" style="opacity: {$hintTransition.opacity};">
+        {hint}
       </div>
     </div>
   </div>
@@ -57,25 +78,28 @@
 
   .drawer {
     width: 100%;
-    min-height: 12rem;
+    min-height: var(--height);
 
     background-color: #222;
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
+
+    box-shadow: 0 0 0.35rem #0004;
   }
 
   .pad {
     position: relative;
-    padding: 2rem 2rem 1rem 1.5rem;
+    padding: 2rem 2rem 0.65rem 1.5rem;
   }
 
   .hint {
     font-family: 'Saira Condensed', sans-serif;
     text-transform: uppercase;
-    color: #fff;
+    color: #fff7;
     font-size: 0.9rem;
     line-height: 135%;
+    margin-top: 0.15rem;
   }
 
 </style>
