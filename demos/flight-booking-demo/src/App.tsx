18e6ba784c02d15b422732c0b1ad38558bb82667
Juho Kilpikoski <juho@speechly.com>
2021-11-03 14:11:24 +0200

    Add demos to rush

--- demos/flight-booking-demo/src/App.tsx
+++ demos/flight-booking-demo/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PushToTalkButton, ErrorPanel } from '@speechly/react-ui';
-import { TranscriptDrawer } from '@speechly/react-ui/components/TranscriptDrawer';
+import { TranscriptDrawer } from '@speechly/react-ui/lib/components/TranscriptDrawer';
 import BookingForm from './components/BookingForm';
 import './voice-form-component-calendar.css';
 import './voice-form-theme-capsule.css';
