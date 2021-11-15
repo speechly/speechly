1b06bea05e81dc57ebcb5b8576ce9cd665a40a12
Ari Nykänen <ari.o.nykanen@gmail.com>
2021-11-15 11:16:35 +0200

    Feature/smart home demo (#81)
    * Initial commit
    
    * Second commit
    
    * Using npm react-ui
    
    * Added working files for art#
    
    * Graphic visualisation of the apartment
    
    * Both DeviceImages and Devices rendered
    
    * Text positioning fixes
    
    * Safari layout fixes
    
    * Voice command snippet support. Using local copy of react-ui.
    
    * SVG switch on Device labels
    
    * Mobile placement fixes
    
    * Preliminary desktop/mobile compatible layout
    
    * Zoom and pan using react-map-interaction
    
    * Min and max scales
    
    * Zoom limits
    
    * Error panel, HttpsRedirect
    
    * Server, HttpsRedirect
    
    * Visualization tweaks
    
    * Added multi-room and multi-device targeting
    
    * Tentative state is made a bit more robust. For early response, the selected room/device is shown upon tentative entity; however tentative app state only changes upon final entities. Visualisation follows the tentative state.
    
    * Html meta
    
    * Support for all devices selector - sets the selected devices array to contain all device names
    
    * Last turn on/off intent is recalled if not specified
    
    * Forgetting timer applied to intent. BigTranscript displays final entities with underline
    
    * Webp images
    
    * Using webp images if client supported
    
    * Migrated to 1.1.0+ version of react-ui
    
    * Migrated to 1.1.0+ version of react-ui
    
    * Timeout fixes; target all rooms if no room specified
    
    * Trying out improvements in PushToTalkButton
    
    * Changed display: hidden to visibility: hidden
    
    * react ui component bump
    
    * react ui component bump
    
    * react ui component bump
    
    * react ui component bump
    
    * Optimized images
    
    * Fixed compile problem
    
    * updated README.md
    
    * Updated to latest react-ui
    
    * Added zoom, zoomPan and backgroundColor query params.
    
    * Preliminary content centering
    
    * Fixed react-map-interaction zoom origin problem by embedding the code in the project and allowing definition of viewport origin
    
    * Restored safeguard https redirect. Removed unnecessary div from App
    
    * v1.0.0 with analytics and react-client 0.0.5
    
    * README update
    
    * v1.0.1 with appName and appVersion added to intents
    
    * More reliable knob animation
    
    * Added instructions
    
    * Readme
    
    * Added express
    
    * v1.0.2
    
    * Readme
    
    * Add meta data for social media posts
    
    To improve shareability
    
    * Update index.html
    
    * Add share image
    
    * Update index.html
    
    * Fixed a meta tag. Updated react-client to latest 0.0.16
    
    * Removed local copy of react-ui
    
    * Added react-client dependency
    
    * Using react-ui from npm
    
    * Reacting on tentative entites if both room and device mentioned
    
    * add http headers for cross origin isolation to use SharedArrayBuffers
    
    * Added Speechly starting event. GA compatible events.
    
    * Updgraded Segment snippet to fix crossorigin problems.
    
    * Code cleanups
    
    * Typography tweaks
    
    * Cleaned up dependencies
    
    * Added smart-home to rush.json. Harmonized dependencies & ran rush update.
    
    * Fixed a missing dependency.
    
    * Refactored App.tsx into App and FloorPlan
    
    * README updates
    
    * Suppressed warnings.
    
    * Readme update
    
    * Rush update
    
    Co-authored-by: Ari Nykänen <ari@speechly.com>
    Co-authored-by: Ottomatias Peura <opeura@gmail.com>
    Co-authored-by: Igor Poltavskiy <igorp@speechly.com>

---
+++ demos/smart-home/src/App.tsx
@@ -0,0 +1,44 @@
+import {
+  BigTranscript,
+  BigTranscriptContainer,
+  PushToTalkButton,
+  PushToTalkButtonContainer,
+  ErrorPanel,
+} from "@speechly/react-ui";
+import PanContainer from "./components/PanContainer";
+import QueryString from "query-string";
+import FloorPlan from "FloorPlan";
+
+// http://localhost:3000/?backgroundColor=%23ff00ff&zoom=0.5&zoomPan=false
+const queryParams = {
+  backgroundColor: "#CEDCEE",
+  ...QueryString.parse(window.location.search), // This suffices for strings
+  zoom: Number(QueryString.parse(window.location.search).zoom || 0.9),
+  zoomPan: !(QueryString.parse(window.location.search).zoomPan === "false"),
+};
+
+export default function App() {
+  return (
+    <div className="App" style={{ backgroundColor: queryParams.backgroundColor }}>
+      <BigTranscriptContainer>
+        <BigTranscript />
+      </BigTranscriptContainer>
+      <PushToTalkButtonContainer>
+        <ErrorPanel />
+        <PushToTalkButton captureKey=" " powerOn={true} intro="" />
+      </PushToTalkButtonContainer>
+      <PanContainer
+        minScale={0.5}
+        maxScale={3.0}
+        disableZoom={!queryParams.zoomPan}
+        disablePan={!queryParams.zoomPan}
+        defaultValue={{
+          scale: queryParams.zoom,
+          translation: { x: 0, y: 0 },
+        }}
+      >
+        <FloorPlan />
+      </PanContainer>
+    </div>
+  );
+}
---
+++ demos/smart-home/src/index.tsx
@@ -0,0 +1,24 @@
+import { SpeechProvider } from '@speechly/react-client';
+import { LogKit } from "@speechly/logkit"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import HttpsRedirect from "./components/HttpsRedirect";
+import './index.css';
+
+const appId = process.env.REACT_APP__SPEECHLY_APP_ID || "738ec39c-3a5c-435f-aa5a-4d815a3e8d87"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HttpsRedirect>
+      <SpeechProvider
+        appId={appId}
+      >
+        <LogKit appName="smart-home" appVersion={104}>
+          <App />
+        </LogKit>
+      </SpeechProvider>
+    </HttpsRedirect>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
