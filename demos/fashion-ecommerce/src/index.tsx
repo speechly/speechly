6d84883d4744acc77289255dba917f962de3108f
Mathias Lindholm <mathias@speechly.com>
2021-11-16 10:48:02 +0200

    Move DemoNavigation inside App to avoid double scroll issue in fashion demo

--- demos/fashion-ecommerce/src/index.tsx
+++ demos/fashion-ecommerce/src/index.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { DemoNavigation } from '@speechly/demo-navigation';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <DemoNavigation />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
