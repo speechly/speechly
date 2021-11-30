9dda92c79429360cc9a01fb9c2f5a1129ca25a16
Mathias Lindholm <mathias@speechly.com>
2021-12-01 00:20:10 +0200

    Make the UI look like a IM chat app

--- demos/speech-to-text/src/App.tsx
+++ demos/speech-to-text/src/App.tsx
@@ -5,27 +5,27 @@ import {
   PushToTalkButtonContainer,
   ErrorPanel
 } from '@speechly/react-ui'
-import textareaBg from './textarea.png'
-import textareaButton from './textarea-button.png'
 import './App.css'
 
 const App: React.FC = (): JSX.Element => {
-
   return (
     <div className="App">
-      <PushToTalkButtonContainer voffset="calc(1rem + 4vh)" size="5rem">
-        <PushToTalkButton size="5rem" captureKey=" " />
-        <ErrorPanel />
-      </PushToTalkButtonContainer>
+      <ErrorPanel />
       <SpeechlyApp/>
     </div>
   )
 }
 
+type Message = {
+  content: string;
+  date: Date;
+};
+
 const SpeechlyApp: React.FC = (): JSX.Element => {
 
   const [textContent, setTextContent] = useState<string>('')
   const [tentativeTextContent, setTentativeTextContent] = useState<string>('')
+  const [messages, setMessages] = useState<Message[]>([])
   const { segment } = useSpeechContext()
 
   const setText = (value: string) => {
@@ -42,14 +42,59 @@ const SpeechlyApp: React.FC = (): JSX.Element => {
         setTextContent(alteredTextContent)
       }
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [segment])
 
+  const sendMessage = () => {
+    const newMessage: Message = {
+      content: textContent,
+      date: new Date()
+    }
+    setMessages([ ...messages, newMessage ])
+    setText('')
+  }
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (textContent.trim() === '') return
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      sendMessage();
+    }
+  }
+
+  const formatTime = (date: Date) => date.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit'})
 
   return (
-    <div className="TextareaContainer" style={{ backgroundImage: `url(${textareaBg})` }}>
-      <textarea onChange={e => setText(e.target.value)} value={tentativeTextContent.toLowerCase()} />
-      {tentativeTextContent && <div className="SendButton" onClick={()=>setText('')} style={{ backgroundImage: `url(${textareaButton})` }} />}
+    <div className="ImApp">
+      <div className="Header">
+        <img className="Header__image" src="https://avatars.githubusercontent.com/u/25465412?s=200&v=4" alt="profile" />
+        <div>
+          <h2 className="Header__title">Speechly demo chat</h2>
+          <div className="Header__meta">42 uses in the chat</div>
+        </div>
+      </div>
+      <div className="Messages">
+        <div className="Message Message--other">
+          <span className="Message__content">Hey 👋 What are you up to?</span>
+          <span className="Message__time">{formatTime(new Date())}</span>
+        </div>
+        {messages.map(m =>
+          <div className="Message Message--me">
+            <span className="Message__content">{m.content}</span>
+            <span className="Message__time">{formatTime(m.date)}</span>
+          </div>
+        )}
+      </div>
+      <div className="Textarea__container">
+        <textarea
+          className="Textarea"
+          placeholder="Speak or type a message…"
+          onChange={e => setText(e.target.value)}
+          value={tentativeTextContent}
+          onKeyPress={e => handleKeyPress(e)}
+          rows={textContent.split('\n').length}
+        />
+        <PushToTalkButton size="56px" intro="" />
+      </div>
     </div>
   )
 }
