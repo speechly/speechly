bca45b58689f718769196cbecf98b5acdba1da2a
Mathias Lindholm <mathias@speechly.com>
2022-01-12 11:58:57 +0200

    Cleanup

--- demos/voice-search/src/App.tsx
+++ demos/voice-search/src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { DemoNavigation } from '@speechly/demo-navigation'
-import { ErrorPanel, BigTranscript } from '@speechly/react-ui'
-import { Input } from './Input'
-import './App.css'
-import avatar from './assets/avatar.png'
-import logo from './assets/logo.svg'
-import searchIcon from './assets/search.svg'
-import imageIcon from './assets/image.svg'
-import videoIcon from './assets/video.svg'
-import newsIcon from './assets/news.svg'
+import React from "react";
+import { DemoNavigation } from "@speechly/demo-navigation";
+import { ErrorPanel, BigTranscript } from "@speechly/react-ui";
+import { Input } from "./Input";
+import "./App.css";
+import avatar from "./assets/avatar.png";
+import logo from "./assets/logo.svg";
+import searchIcon from "./assets/search.svg";
+import imageIcon from "./assets/image.svg";
+import videoIcon from "./assets/video.svg";
+import newsIcon from "./assets/news.svg";
 
 const App: React.FC = (): JSX.Element => {
   return (
@@ -18,7 +18,7 @@ const App: React.FC = (): JSX.Element => {
         <BigTranscript highlightColor="#009FFA" backgroundColor="#1F2D3B" />
       </div>
       <ErrorPanel />
-      <SearchApp/>
+      <SearchApp />
     </div>
   )
 }
--- demos/voice-search/src/Input.tsx
+++ demos/voice-search/src/Input.tsx
@@ -1,64 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import { useSpeechContext } from '@speechly/react-client'
-import { PushToTalkButton } from '@speechly/react-ui'
-import './Input.css'
-import searchIconGray from './assets/search-gray.svg'
-import closeIconGray from './assets/close-gray.svg'
+import React, { useEffect, useState } from "react";
+import { useSpeechContext } from "@speechly/react-client";
+import { PushToTalkButton } from "@speechly/react-ui";
+import "./Input.css";
+import searchIconGray from "./assets/search-gray.svg";
+import closeIconGray from "./assets/close-gray.svg";
 
 export const Input: React.FC <{
   small?: boolean,
   query?: string,
 }> = ({ small, query }) => {
-  const [textContent, setTextContent] = useState<string>('')
-  const [tentativeTextContent, setTentativeTextContent] = useState<string>('')
-  const { segment } = useSpeechContext()
+  const [textContent, setTextContent] = useState<string>("");
+  const [tentativeTextContent, setTentativeTextContent] = useState<string>("");
+  const { segment } = useSpeechContext();
 
   const setText = (value: string) => {
-    setTextContent(value)
-    setTentativeTextContent(value)
+    setTextContent(value);
+    setTentativeTextContent(value);
   }
 
   const toSentenceCase = (str: string) => {
-    const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-    const punctuation = ['.','!','?']
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    const punctuation = [".","!","?"]
     alphabet.forEach(letter => {
       punctuation.forEach(punc => {
-          str = str.replace(`${punc} ${letter.toLowerCase()}`,`${punc} ${letter}`)
-      })
-    })
+          str = str.replace(`${punc} ${letter.toLowerCase()}`,`${punc} ${letter}`);
+      });
+    });
     return str.charAt(0).toUpperCase() + str.substr(1);
   }
 
   useEffect(() => {
     if (segment) {
-      const plainString = segment.words.filter(w => w.value).map(w => w.value).join(' ')
-      const alteredTextContent = textContent ? [textContent, plainString].join(' ') : plainString
+      const plainString = segment.words.filter(w => w.value).map(w => w.value).join(" ");
+      const alteredTextContent = textContent ? [textContent, plainString].join(" ") : plainString
       const formattedTextContent = alteredTextContent
-        .replace(' COMMA', ',')
-        .replace(' PERIOD', '.')
-        .replace(' DOT', '.')
-        .replace(' QUESTION MARK', '?')
-        .replace(' EXCLAMATION MARK', '!')
-        .replace(' EXCLAMATION POINT', '!')
-        .replace(' COLON', ':')
-        .replace(' SEMICOLON', ';')
-        .replace(' SEMI COLON', ';')
+        .replace(" COMMA", ",")
+        .replace(" PERIOD", ".")
+        .replace(" DOT", ".")
+        .replace(" QUESTION MARK", "?")
+        .replace(" EXCLAMATION MARK", "!")
+        .replace(" EXCLAMATION POINT", "!")
+        .replace(" COLON", ":")
+        .replace(" SEMICOLON", ";")
+        .replace(" SEMI COLON", ";")
         .toLowerCase()
       const casedTextContent = toSentenceCase(formattedTextContent)
-      setTentativeTextContent(casedTextContent)
+      setTentativeTextContent(casedTextContent);
       if (segment.isFinal) {
-        setTextContent(casedTextContent)
+        setTextContent(casedTextContent);
       }
     }
   // eslint-disable-next-line
   }, [segment])
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value)
+    setText(e.target.value);
   }
 
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter' && !e.shiftKey) {
+    if (e.key === "Enter" && !e.shiftKey) {
       e.preventDefault();
     }
   }
