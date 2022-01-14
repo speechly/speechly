9c678cbc43f3c94ac50a99fbb938e0bda291d9f3
Mathias Lindholm <mathias@speechly.com>
2022-01-14 08:42:06 +0200

    Update demos and readmes (#125)
    * Resize ogshare images from 3200 -> 1600
    
    * Send message directly when isFinal
    
    * Add small chatbot thingy
    
    * Update demo readmes

--- demos/speech-to-text/src/App.tsx
+++ demos/speech-to-text/src/App.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import { useSpeechContext } from '@speechly/react-client'
-import { DemoNavigation } from '@speechly/demo-navigation'
-import { PushToTalkButton, ErrorPanel } from '@speechly/react-ui'
-import './App.css'
+import React, { useCallback, useEffect, useState } from "react";
+import { useSpeechContext } from "@speechly/react-client";
+import { DemoNavigation } from "@speechly/demo-navigation";
+import { PushToTalkButton, ErrorPanel } from "@speechly/react-ui";
+import "./App.css";
 
 const App: React.FC = (): JSX.Element => {
   return (
@@ -14,14 +14,17 @@ const App: React.FC = (): JSX.Element => {
   )
 }
 
+type Sender = "other" | "me";
+
 type Message = {
+  sender: Sender;
   content: string;
   date: Date;
 };
 
 const SpeechlyApp: React.FC = (): JSX.Element => {
-  const [textContent, setTextContent] = useState<string>('')
-  const [tentativeTextContent, setTentativeTextContent] = useState<string>('')
+  const [textContent, setTextContent] = useState<string>("")
+  const [tentativeTextContent, setTentativeTextContent] = useState<string>("")
   const [messages, setMessages] = useState<Message[]>([])
   const { segment } = useSpeechContext()
 
@@ -31,8 +34,8 @@ const SpeechlyApp: React.FC = (): JSX.Element => {
   }
 
   const toSentenceCase = (str: string) => {
-    const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-    const punctuation = ['.','!','?']
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    const punctuation = [".","!","?"]
     alphabet.forEach(letter => {
       punctuation.forEach(punc => {
           str = str.replace(`${punc} ${letter.toLowerCase()}`,`${punc} ${letter}`)
@@ -41,71 +44,81 @@ const SpeechlyApp: React.FC = (): JSX.Element => {
     return str.charAt(0).toUpperCase() + str.substr(1);
   }
 
+  const sendMessage = useCallback((content: string, sender: Sender) => {
+    if (content.trim() === "") return
+    const newMessage: Message = {
+      sender,
+      content,
+      date: new Date()
+    }
+    setMessages([ ...messages, newMessage ]);
+    if (sender === "me") setText("");
+  }, [messages])
+
   useEffect(() => {
     if (segment) {
-      const plainString = segment.words.filter(w => w.value).map(w => w.value).join(' ')
-      const alteredTextContent = textContent ? [textContent, plainString].join(' ') : plainString
+      const plainString = segment.words.filter(w => w.value).map(w => w.value).join(" ")
+      const alteredTextContent = textContent ? [textContent, plainString].join(" ") : plainString
       const formattedTextContent = alteredTextContent
-        .replace(' COMMA', ',')
-        .replace(' PERIOD', '.')
-        .replace(' DOT', '.')
-        .replace(' QUESTION MARK', '?')
-        .replace(' EXCLAMATION MARK', '!')
-        .replace(' EXCLAMATION POINT', '!')
-        .replace(' COLON', ':')
-        .replace(' SEMICOLON', ';')
-        .replace(' SEMI COLON', ';')
+        .replace(" COMMA", ",")
+        .replace(" PERIOD", ".")
+        .replace(" DOT", ".")
+        .replace(" QUESTION MARK", "?")
+        .replace(" EXCLAMATION MARK", "!")
+        .replace(" EXCLAMATION POINT", "!")
+        .replace(" COLON", ":")
+        .replace(" SEMICOLON", ";")
+        .replace(" SEMI COLON", ";")
         .toLowerCase()
-      const casedTextContent = toSentenceCase(formattedTextContent)
-      setTentativeTextContent(casedTextContent)
+      const casedTextContent = toSentenceCase(formattedTextContent);
+      setTentativeTextContent(casedTextContent);
       if (segment.isFinal) {
-        setTextContent(casedTextContent)
+        setText(casedTextContent);
+        sendMessage(casedTextContent, "me");
       }
     }
   // eslint-disable-next-line
   }, [segment])
 
-  const sendMessage = () => {
-    if (textContent.trim() === '') return
-    const newMessage: Message = {
-      content: textContent,
-      date: new Date()
+  useEffect(() => {
+    if (!messages.length) {
+      const timer = setTimeout(() => sendMessage("Hey 👋 What are you up to?", "other"), 1500);
+      return () => clearTimeout(timer);
     }
-    setMessages([ ...messages, newMessage ])
-    setText('')
-  }
+    if (messages.length === 2) {
+      const timer = setTimeout(() => sendMessage("That sounds great! 👍", "other"), 1500);
+      return () => clearTimeout(timer);
+    }
+  }, [messages, sendMessage])
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setText(e.target.value)
+    setText(e.target.value);
   }
 
   const handleKeyPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === 'Enter' && !e.shiftKey) {
+    if (e.key === "Enter" && !e.shiftKey) {
       e.preventDefault();
-      sendMessage();
+      sendMessage(textContent, "me");
     }
   }
 
-  const formatTime = (date: Date) => date.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit'})
+  const formatTime = (date: Date) => date.toLocaleTimeString("en-US", {hour: "2-digit", minute:"2-digit"})
 
   return (
     <div className="ImApp">
       <div className="Header">
         <img className="Header__image" src="https://avatars.githubusercontent.com/u/25465412?s=200&v=4" alt="profile" />
         <div>
-          <h2 className="Header__title">Speechly demo chat</h2>
+          <h2 className="Header__title">Speechly chat demo</h2>
           <div className="Header__meta">42 users in the chat</div>
         </div>
       </div>
       <div className="Messages">
-        <div className="Message Message--other">
-          <span className="Message__content">Hey 👋 What are you up to?</span>
-          <span className="Message__time">{formatTime(new Date())}</span>
-        </div>
-        {messages.map(m =>
-          <div key={m.content} className="Message Message--me">
-            <span className="Message__content">{m.content}</span>
-            <span className="Message__time">{formatTime(m.date)}</span>
+        {messages.map(message =>
+          <div key={message.date.valueOf()} className={`Message Message--${message.sender}`}>
+            {message.sender === "other" && <span className="Message__sender">bot</span>}
+            <span className="Message__content">{message.content}</span>
+            <span className="Message__time">{formatTime(message.date)}</span>
           </div>
         )}
       </div>
@@ -119,11 +132,15 @@ const SpeechlyApp: React.FC = (): JSX.Element => {
             onKeyPress={handleKeyPress}
             rows={1}
           />
-          <button disabled={!textContent} className="SendButton" onClick={sendMessage}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3.4 20.4l17.45-7.48c.81-.35.81-1.49 0-1.84L3.4 3.6c-.66-.29-1.39.2-1.39.91L2 9.12c0 .5.37.93.87.99L17 12 2.87 13.88c-.5.07-.87.5-.87 1l.01 4.61c0 .71.73 1.2 1.39.91z"/></svg>
-          </button>
         </div>
-        <PushToTalkButton size="56px" intro="" />
+        <div className="Textarea__button">
+          <PushToTalkButton
+            gradientStops={["#508CFF", "#009FFA", "#00E48F"]}
+            size="56px"
+            showTime={2000}
+            tapToTalkTime={0}
+            />
+          </div>
         </div>
     </div>
   )
