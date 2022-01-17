0642223539a06788ea21a7aecb7c13a64e0febe1
Ari Nykänen <ari.o.nykanen@gmail.com>
2022-01-17 11:58:08 +0200

    Twin es and umd builds

--- libraries/browser-client/rollup.config.js
+++ libraries/browser-client/rollup.config.js
@@ -2,26 +2,39 @@ import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
-const typeScriptDefaults = {
+const esmBundle = {
+  input: 'src/index.ts',
+
   output: {
+    name: 'Speechly',
     format: 'esm', // ES Modules
     sourcemap: true,
+    file: 'dist/speechly.es.js',
   },
   plugins: [
     commonjs(),
-    typescript({ tsconfig: './tsconfig.json' }),
     nodeResolve(),
+    typescript({ tsconfig: './tsconfig.json' }),
+  ],
+};
+
+const umdBundle = {
+  input: 'src/index.ts',
+
+  output: {
+    name: 'Speechly',
+    format: 'umd',
+    sourcemap: true,
+    file: 'dist/speechly.umd.js',
+  },
+  plugins: [
+    commonjs(),
+    nodeResolve(),
+    typescript({ tsconfig: './tsconfig.json' }),
   ],
 };
 
 export default [
-  {...typeScriptDefaults,
-    input: 'src/index.ts',
-    output: [
-      {
-        ...typeScriptDefaults.output,
-        file: 'dist/index.js',
-      },
-    ],
-  },
+  esmBundle,
+  umdBundle,
 ]
