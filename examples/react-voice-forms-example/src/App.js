209d6d1b19c9a354be39e025d1742a6dfa2a17b0
Ari Nykänen <ari.o.nykanen@gmail.com>
2022-01-26 10:26:22 +0200

    example styles

--- examples/react-voice-forms-example/src/App.js
+++ examples/react-voice-forms-example/src/App.js
@@ -6,7 +6,7 @@ import {
   ErrorPanel
 } from "@speechly/react-ui";
 import { VoiceInput, VoiceSelect, VoiceToggle } from '@speechly/react-voice-forms'
-import '@speechly/react-voice-forms/css/theme/mui.css'
+import '@speechly/react-voice-forms/css/theme/capsule.css'
 
 function App() {
   const { segment } = useSpeechContext()
@@ -28,12 +28,15 @@ function App() {
       <PushToTalkButton placement="bottom" captureKey=" "/>
       <ErrorPanel placement="bottom"/>
       <p className="openconsole">ℹ️ Open the Browser Console to see speech segment outputs</p>
-      <VoiceInput label="Phone number" changeOnEntityType="phone_number" value={phone} onChange={setPhone}/>
-      <VoiceSelect label="Phone number" changeOnEntityType="phone_number" value={phone} options={["", "112 ", "911 "]} onChange={setPhone}/>
-      <VoiceToggle changeOnEntityType="phone_number" value={phone} options={["", "112 ", "911 "]} onChange={setPhone}/>
-      <div>Phone number state: {phone}</div>
-      <button onClick={() => setPhone("")}>Reset</button>
-      <button onClick={() => setPhone("911 ")}>Set to 911</button>
+
+      <main>
+        <VoiceInput label="Phone number" changeOnEntityType="phone_number" value={phone} onChange={setPhone}/>
+        <VoiceSelect label="Phone number" changeOnEntityType="phone_number" value={phone} options={["", "112 ", "911 "]} onChange={setPhone}/>
+        <VoiceToggle changeOnEntityType="phone_number" value={phone} options={["112 ", "911 "]} onChange={setPhone}/>
+        <div>Phone number state: '{phone}'. Manipulate state:</div>
+        <button onClick={() => setPhone("")}>Reset</button>
+        <button onClick={() => setPhone("911 ")}>Set to 911</button>
+      </main>
     </div>
   );
 }
