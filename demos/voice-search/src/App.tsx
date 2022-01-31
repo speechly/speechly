2a40e484aa19746d5284548898e00bb7bed7c865
Mathias Lindholm <mathias@speechly.com>
2022-01-31 12:07:30 +0200

    Extract isStandalone into an utility function

--- demos/voice-search/src/App.tsx
+++ demos/voice-search/src/App.tsx
@@ -3,13 +3,14 @@ import { DemoNavigation } from "@speechly/demo-navigation";
 import { ErrorPanel } from "@speechly/react-ui";
 import { SearchContextProvider } from "./context";
 import SearchView from "./SearchView";
+import { isStandalone } from "./utils";
 import "./App.css"
 
 const App: React.FC = (): JSX.Element => {
   return (
     <SearchContextProvider>
       <div className="App">
-        { process.env.REACT_APP_VOICE_SEARCH_DEPLOYMENT !== 'STANDALONE' && <DemoNavigation /> }
+        { !isStandalone && <DemoNavigation /> }
         <ErrorPanel />
         <SearchView />
       </div>
