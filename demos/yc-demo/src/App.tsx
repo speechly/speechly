e19e02f55245a40f69a718accc6525f11807c545
Mathias Lindholm <mathias@speechly.com>
2022-03-15 10:57:48 +0200

    Add new demo

---
+++ demos/yc-demo/src/App.tsx
@@ -0,0 +1,9 @@
+import React, { useCallback, useEffect, useState, useRef } from "react";
+
+const App = () => {
+  return (
+    <div className="App">Hello world</div>
+  );
+}
+
+export default App
