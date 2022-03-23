9b3504315c79d5beb7363ac4f483f63c6e79dbfd
Mathias Lindholm <mathias@speechly.com>
2022-03-23 12:23:47 +0200

    Add demo folder

---
+++ demos/moderation/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { DemoNavigation } from "@speechly/demo-navigation";
+import "./App.css";
+
+
+const App: React.FC = (): JSX.Element => {
+  return (
+    <div className="App">
+      <DemoNavigation />
+    </div>
+  )
+}
+export default App
---
+++ demos/moderation/src/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { SpeechProvider } from "@speechly/react-client";
+import * as serviceWorker from "./serviceWorker";
+import { LogKit } from "@speechly/logkit";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SpeechProvider appId="6f1c7eaa-53fa-495e-9319-4ceacfa88cfe">
+      <LogKit appName="moderation" appVersion={100} >
+        <App />
+      </LogKit>
+    </SpeechProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
