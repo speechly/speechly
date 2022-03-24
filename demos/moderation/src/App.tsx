e5dadf29037dfa265dffdd3bbe6b6a005047e882
Mathias Lindholm <mathias@speechly.com>
2022-03-24 15:16:00 +0200

    Convert header items into Cover component and hook up audio player

--- demos/moderation/src/App.tsx
+++ demos/moderation/src/App.tsx
@@ -1,59 +1,105 @@
+import React, { useState } from "react";
 import Plyr from "plyr-react";
-import React from "react";
+import classNames from "classnames";
 import 'plyr-react/dist/plyr.css';
 import "./App.css";
 
-const audioSrc: Plyr.SourceInfo = {
-  type: 'audio',
-  sources: [
-    {
-      src: 'https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3',
+const items: {
+  audioSrc: Plyr.SourceInfo;
+  title: string;
+  duration: string;
+  thumbnail: string;
+}[] = [
+  {
+    audioSrc: {
+      type: "audio",
+      sources: [{ src: "audio/sample-3s.mp3" }]
     },
-  ],
-};
+    title: "Title goes here",
+    duration: "0:03",
+    thumbnail: "https://i.ytimg.com/vi/pyTHNeRAFwo/mqdefault.jpg"
+  },
+  {
+    audioSrc: {
+      type: "audio",
+      sources: [{ src: "audio/sample-15s.mp3" }]
+    },
+    title: "Title goes here",
+    duration: "0:19",
+    thumbnail: "https://i.ytimg.com/vi/pyTHNeRAFwo/mqdefault.jpg"
+  },
+  {
+    audioSrc: {
+      type: "audio",
+      sources: [{ src: "audio/sample-9s.mp3" }]
+    },
+    title: "Title goes here",
+    duration: "0:09",
+    thumbnail: "https://i.ytimg.com/vi/pyTHNeRAFwo/mqdefault.jpg"
+  },
+  {
+    audioSrc: {
+      type: "audio",
+      sources: [{ src: "audio/sample-12s.mp3" }]
+    },
+    title: "Title goes here",
+    duration: "0:12",
+    thumbnail: "https://i.ytimg.com/vi/pyTHNeRAFwo/mqdefault.jpg"
+  },
+]
 
 const playerOptions: Plyr.Options = {
   controls: ['play', 'progress', 'current-time', 'mute', 'volume'],
 };
 
-const App: React.FC = (): JSX.Element => {
+
+type CoverProps = {
+  title: string;
+  duration: string;
+  thumbnail: string;
+  isSelected: boolean;
+  onClick: () => void;
+};
+
+const Cover = ({ title, duration, thumbnail, isSelected, onClick } : CoverProps) => {
+  const classes = classNames({
+    Item: true,
+    "Item--selected": isSelected
+  });
+
+  return (
+    <div className={classes} onClick={onClick}>
+      <img className="Item__image" src={thumbnail} alt={title} />
+      <div className="Item__info">
+        <span className="Item__title">{title}</span>
+        <span className="Item__duration">{duration}</span>
+      </div>
+    </div>
+  )
+}
+
+const App= () => {
+  const [currentItem, setCurrentItem] = useState(0);
+
   return (
     <div className="App">
       <div className="Header">
         <div className="Header__inner">
-          <div className="Item Item--selected">
-            <img className="Item__image" src="https://i.ytimg.com/vi/pyTHNeRAFwo/mqdefault.jpg" alt="description" />
-            <div className="Item__info">
-              <span className="Item__title">Title</span>
-              <span className="Item__duration">0:00</span>
-            </div>
-          </div>
-          <div className="Item">
-            <img className="Item__image" src="https://i.ytimg.com/vi/pyTHNeRAFwo/mqdefault.jpg" alt="description" />
-            <div className="Item__info">
-              <span className="Item__title">Title</span>
-              <span className="Item__duration">0:00</span>
-            </div>
-          </div>
-          <div className="Item">
-            <img className="Item__image" src="https://i.ytimg.com/vi/pyTHNeRAFwo/mqdefault.jpg" alt="description" />
-            <div className="Item__info">
-              <span className="Item__title">Title</span>
-              <span className="Item__duration">0:00</span>
-            </div>
-          </div>
-          <div className="Item">
-            <img className="Item__image" src="https://i.ytimg.com/vi/pyTHNeRAFwo/mqdefault.jpg" alt="description" />
-            <div className="Item__info">
-              <span className="Item__title">Title</span>
-              <span className="Item__duration">0:00</span>
-            </div>
-          </div>
+          {items.map((item, index) =>
+            <Cover
+              key={index}
+              title={item.title}
+              duration={item.duration}
+              thumbnail={item.thumbnail}
+              isSelected={index === currentItem}
+              onClick={() => setCurrentItem(index)}
+            />
+          )}
         </div>
       </div>
       <div className="Player">
         <div className="Player__inner">
-          <Plyr source={audioSrc} options={playerOptions} />
+          <Plyr source={items[currentItem].audioSrc} options={playerOptions} />
         </div>
       </div>
       <div className="Content">
