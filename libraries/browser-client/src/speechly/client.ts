6f98b1d5a3241b2b6b0fdf22dd3cbf2dbe7e213d
Antti Ukkonen <antti@speechly.com>
2022-03-25 18:09:21 +0200

    simple audio file upload in browser client

--- libraries/browser-client/src/speechly/client.ts
+++ libraries/browser-client/src/speechly/client.ts
@@ -627,6 +627,39 @@ export class Client {
   public printStats(): void {
     this.microphone.printStats()
   }
+
+  public sendAudioData(audioData: ArrayBuffer): void {
+    console.log('this.audioContext = ', this.audioContext)
+    console.log(audioData)
+    const audioBuffer = await this.audioContext.decodeAudioData(audioData)
+    const samples = audioBuffer.getChannelData(0)
+
+    this.listening = true
+
+    this.setState(ClientState.Starting)
+    const contextId = await this.apiClient.startContext()
+    console.log('contextid =', contextId)
+    this.activeContexts.set(contextId, new Map<number, SegmentState>())
+    this.setState(ClientState.Recording)
+
+    let sendBuffer: Float32Array
+    for (let b = 0; b < samples.length; b += 16000) {
+      const e = b + 16000
+      if (e > samples.length) {
+        sendBuffer = samples.slice(b)
+      } else {
+        sendBuffer = samples.slice(b, e)
+      }
+      this.apiClient.sendAudio(sendBuffer)
+    }
+
+    this.listening = false
+
+    this.setState(ClientState.Stopping)
+    await this.apiClient.stopContext()
+    this.activeContexts.delete(contextId)
+    this.setState(ClientState.Connected)
+  }
 }
 
 function generateWsUrl(baseUrl: string, sampleRate: number): string {
