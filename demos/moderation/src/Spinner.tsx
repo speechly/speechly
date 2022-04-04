dfd2e5053d7cf83cc47d3f77bc85a318936fb4e1
Mathias Lindholm <mathias@speechly.com>
2022-04-04 15:24:14 +0300

    Add "processing" message to Spinner

--- demos/moderation/src/Spinner.tsx
+++ demos/moderation/src/Spinner.tsx
@@ -7,6 +7,7 @@ export const Spinner = () => {
       <svg viewBox="0 0 50 50">
         <circle cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
       </svg>
+      <p>Processing audio…</p>
     </div>
   )
 }
