c66ad2535c79e56885a3e553b74586d5ccb21c4f
Ari Nykänen <ari.o.nykanen@gmail.com>
2022-04-27 12:23:51 +0300

    Feature/next js example (#176)
    * Added next-js-example project with PTT button and TranscriptDrawer.
    
    * Moved SpeechProvider to _app.tsx. Added IntroPopup component.
    
    * Added about.tsx and page navigation using Link

---
+++ examples/next-js-example/pages/_app.tsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { SpeechProvider } from '@speechly/react-client'
+import { PushToTalkButton, TranscriptDrawer, IntroPopup } from '@speechly/react-ui'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <SpeechProvider appId="98e16d5b-4e2f-4b76-8680-1b98959ddeee">
+      <PushToTalkButton placement='bottom'></PushToTalkButton>
+      <TranscriptDrawer></TranscriptDrawer>
+      <IntroPopup></IntroPopup>
+      <Component {...pageProps} />
+    </SpeechProvider>
+  )
+}
+
+export default MyApp
