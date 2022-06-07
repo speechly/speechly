ed9b9ad96bd6eab3850bb57fcbef6583dd3cc820
Ari Nykänen <ari.o.nykanen@gmail.com>
2022-06-07 17:23:15 +0300

    Intro-popup initialized microphone thru a customEvent instead of a browser window property.

--- libraries/react-ui/src/components/IntroPopup.tsx
+++ libraries/react-ui/src/components/IntroPopup.tsx
@@ -1,3 +1,4 @@
+import { MessageType } from '@speechly/browser-ui'
 import { useSpeechContext } from '@speechly/react-client'
 import React, { useEffect, useState } from 'react'
 
@@ -28,8 +29,25 @@ export const IntroPopup: React.FC<IntroPopupProps> = ({
   appId,
   children,
 }) => {
-  const { clientState } = useSpeechContext()
+  const { clientState, microphoneState, initialize } = useSpeechContext()
   const [loaded, setLoaded] = useState(false)
+  let refElement: HTMLElement | null = null
+
+  const initMic = (): void => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    (async() => {
+      await initialize()
+    })()
+  }
+
+  const setRef = (el: HTMLElement): void => {
+    if (el !== null) {
+      refElement = el
+      refElement.addEventListener(MessageType.requeststartmicrophone, initMic)
+    } else if (refElement !== null) {
+      refElement.removeEventListener(MessageType.requeststartmicrophone, initMic)
+    }
+  }
 
   // Dynamic import of HTML custom element to play nice with Next.js SSR
   useEffect(() => {
@@ -43,7 +61,7 @@ export const IntroPopup: React.FC<IntroPopupProps> = ({
   if (!loaded) return null
 
   return (
-    <intro-popup clientstate={clientState} appid={appId}>
+    <intro-popup ref={setRef} clientstate={clientState} microphoneState={microphoneState} appid={appId}>
       {children}
     </intro-popup>
   )
