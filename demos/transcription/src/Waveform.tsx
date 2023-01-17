3b537678fb51733b375e7f28845dcb3b809e52c9
Mathias Lindholm <mathias@speechly.com>
2023-01-17 11:33:25 +0200

    Add audio duration

--- demos/transcription/src/Waveform.tsx
+++ demos/transcription/src/Waveform.tsx
@@ -4,6 +4,7 @@ import { ReactComponent as Play } from './assets/play.svg';
 import { ReactComponent as Pause } from './assets/pause.svg';
 import { ReactComponent as VolumeUp } from './assets/volume.svg';
 import './Waveform.css';
+import formatDuration from 'format-duration';
 
 interface Props {
   url?: string;
@@ -29,6 +30,8 @@ export const Waveform: React.FC<Props> = ({ url, children, onSeek }) => {
   const wavesurfer: { current: WaveSurfer | null } = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [volume, setVolume] = useState(0.5);
+  const [currentTime, setCurrentTime] = useState<number>();
+  const [duration, setDuration] = useState<number>();
 
   useEffect(() => {
     const options = formWaveSurferOptions(waveformRef.current);
@@ -38,12 +41,14 @@ export const Waveform: React.FC<Props> = ({ url, children, onSeek }) => {
     wavesurfer.current.on('ready', () => {
       if (wavesurfer.current) {
         wavesurfer.current.setVolume(volume);
+        setCurrentTime(1);
+        setDuration(wavesurfer.current.getDuration() * 1000);
       }
     });
     wavesurfer.current.on('play', () => setIsPlaying(true));
     wavesurfer.current.on('pause', () => setIsPlaying(false));
     wavesurfer.current.on('volume', (e) => setVolume(e));
-
+    wavesurfer.current.on('audioprocess', (e) => setCurrentTime(e * 1000));
     wavesurfer.current.on('seek', (e) => {
       const ct = wavesurfer.current?.getCurrentTime();
       if (!ct) return;
@@ -73,7 +78,13 @@ export const Waveform: React.FC<Props> = ({ url, children, onSeek }) => {
       <button onClick={handlePlayPause} className="Waveform__playPause" disabled={!url}>
         {isPlaying ? <Pause width={28} height={28} /> : <Play width={28} height={28} />}
       </button>
+      <div className="Waveform__time" style={{ marginLeft: 8 }}>
+        {currentTime && formatDuration(currentTime)}
+      </div>
       <div className="Waveform__waveform" id="waveform" ref={waveformRef} />
+      <div className="Waveform__time" style={{ marginRight: 8 }}>
+        {duration && formatDuration(duration)}
+      </div>
       <div className="Waveform__volume">
         <VolumeUp />
         <input
