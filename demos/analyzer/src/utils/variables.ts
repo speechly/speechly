f3f2e3f42e838d71b9f53f4befc2a2b0d37596c7
Mathias Lindholm <mathias@speechly.com>
2023-03-08 12:14:47 +0200

    Update api endpoints

--- demos/analyzer/src/utils/variables.ts
+++ demos/analyzer/src/utils/variables.ts
@@ -1,5 +1,5 @@
 export const CHUNK_MS = 2000;
 export const AUDIO_ANALYSIS_CHUNK_SIZE = 16 * CHUNK_MS;
-export const TEXT_CLASSIFIER_URL = 'https://api.speechly.com/text-classifier-api/classify';
-export const AUDIO_CLASSIFIER_URL = 'https://api.speechly.com/text-classifier-api/classifyAudio';
+export const TEXT_CLASSIFIER_URL = 'https://api.speechly.com/text-classifier-api/classify/text';
+export const AUDIO_CLASSIFIER_URL = 'https://api.speechly.com/text-classifier-api/classify/tone';
 export const MAX_TAGS = 8;
