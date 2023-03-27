0196524e35e871e67c25f4015fa6c95141c03efe
Mathias Lindholm <mathias@speechly.com>
2023-03-27 21:31:15 +0300

    Fashion demo fix (#297)
    * Bump react-ui
    
    * Create own mic button as temporary fix

--- demos/fashion-ecommerce/src/App.tsx
+++ demos/fashion-ecommerce/src/App.tsx
@@ -1,13 +1,38 @@
-import { BigTranscript, BigTranscriptContainer, PushToTalkButton, IntroPopup } from "@speechly/react-ui";
-import { DemoNavigation } from "@speechly/demo-navigation";
-import Inventory from "components/Inventory";
-import SmartFilter from "components/SmartFilters";
-import Navigation from "components/Navigation";
-import { AppContextProvider } from "AppContext";
-import "components/BigTransscript.css";
-import "App.css";
+import { useSpeechContext } from '@speechly/react-client';
+import {
+  BigTranscript,
+  BigTranscriptContainer,
+  // PushToTalkButton,
+  // IntroPopup,
+} from '@speechly/react-ui';
+import { DemoNavigation } from '@speechly/demo-navigation';
+import classNames from 'classnames';
+import Inventory from 'components/Inventory';
+import SmartFilter from 'components/SmartFilters';
+import Navigation from 'components/Navigation';
+import { AppContextProvider } from 'AppContext';
+import { ReactComponent as MicIcon } from './res/mic.svg';
+import 'components/BigTransscript.css';
+import 'App.css';
 
 export default function App() {
+  const { attachMicrophone, start, stop, listening, segment } =
+    useSpeechContext();
+
+  const handleClick = async () => {
+    if (listening) {
+      await stop();
+    } else {
+      await attachMicrophone();
+      await start();
+    }
+  };
+
+  const classes = classNames({
+    Microphone: true,
+    'Microphone--active': listening,
+  });
+
   return (
     <AppContextProvider>
       <div className="App">
@@ -16,12 +41,21 @@ export default function App() {
         <SmartFilter />
         <Inventory>
           <BigTranscriptContainer position="absolute">
-            <BigTranscript formatText={false}/>
+            <BigTranscript formatText={false} mockSegment={segment} />
           </BigTranscriptContainer>
         </Inventory>
       </div>
-      <PushToTalkButton captureKey=" " placement="bottom" size="80px" showTime={3000} powerOn="auto" />
-      <IntroPopup></IntroPopup>
+      <button type="button" className={classes} onClick={handleClick}>
+        <MicIcon width={36} height={36} />
+      </button>
+      {/* <PushToTalkButton
+        captureKey=" "
+        placement="bottom"
+        size="80px"
+        showTime={3000}
+        powerOn="auto"
+      />
+      <IntroPopup/> */}
     </AppContextProvider>
   );
 }
