e9887678331d2a082ac94640ead650695ba3df90
Mathias Lindholm <mathias@speechly.com>
2023-05-26 08:06:42 +0300

    Remove old/unused demos (#309)
    * Remove old/unused demos
    
    * Update browserlist ignore to boolean

--- demos/smart-home/src/react-map-interaction/README.md
+++
@@ -1,2 +0,0 @@
-Included from https://github.com/strateos/react-map-interaction under MIT licence
-to work around zoom origin problem
--- demos/smart-home/src/react-map-interaction/index.js
+++
@@ -1,5 +0,0 @@
-import MapInteractionCSS from './MapInteractionCSS';
-import MapInteraction from './MapInteraction';
-
-export { MapInteractionCSS, MapInteraction };
-export default MapInteraction;
